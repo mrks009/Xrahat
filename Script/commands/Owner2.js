2f5d8bee89928045c2186caf3ca73203294c36e0
mrks009 <102096579+mrks009@users.noreply.github.com>
2026-02-24 17:08:12 +0300

    Update owner information in Owner2.js

--- Script/commands/Owner2.js
+++ Script/commands/Owner2.js
@@ -28,7 +28,7 @@ module.exports.run = async function ({ api, event }) {
  body:
 `🌟 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 🌟
 
-👑 𝗡𝗮𝗺𝗲: Rahat Islam😘
+👑 𝗡𝗮𝗺𝗲: Mehedi hasan😘
 😻 𝗔𝗱𝗱𝗿𝗲𝘀𝘀: মেয়েদের মনে🙈
 💼 𝗣𝗿𝗼𝗳𝗲𝘀𝘀𝗶𝗼𝗻: মেয়েদের মন জয় করা😍
 
@@ -39,7 +39,7 @@ module.exports.run = async function ({ api, event }) {
 📱 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽: দিলে আমার আম্মু বকা দিবা 🤣
 👻 𝗦𝗻𝗮𝗽𝗰𝗵𝗮𝘁: তোদের মতো কালা নাকি ফিল্টার লাগামু🤭
 
-🤖 𝗕𝗢𝗧 𝗕𝗬: 🔰 𝗥𝗮𝗵𝗮𝘁_𝗜𝘀𝗹𝗮𝗺 🔰
+🤖 𝗕𝗢𝗧 𝗕𝗬: 🔰 Mehedi Hasan 🔰
 `,
  attachment: fs.createReadStream(path)
  }, event.threadID, () => fs.unlinkSync(path));
